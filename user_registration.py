6ab280e6e8c9ec610f725df56fa3c5b68921b243
Adityax2808 <ii3837@srmist.edu.in>
2025-02-27 17:06:24 +0530

    [Aditya] Add: added function to validate the user's first name

--- user_registration.py
+++ user_registration.py
@@ -1,1 +1,16 @@
-print("Welcome to user registration ")
+import re
+
+def first_name(name):
+    return bool(re.fullmatch(r'^[A-Z][a-zA-Z]{2,}$', name))
+
+def main():
+    first_name = input("Enter your first name: ")
+
+    if first_name(first_name):
+        print("Valid first name!")
+    else:
+        print("Invalid name. First name must start with a capital letter and have at least 3 characters.")
+
+
+if __name__ == "__main__":
+    main()
