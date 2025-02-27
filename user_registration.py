5fa4107d745b938a8b461ebb662684015ee26703
Adityax2808 <ii3837@srmist.edu.in>
2025-02-27 17:08:42 +0530

    [Aditya] Add: added function to validate the user's last name

--- user_registration.py
+++ user_registration.py
@@ -3,13 +3,17 @@ import re
 def first_name(name):
     return bool(re.fullmatch(r'^[A-Z][a-zA-Z]{2,}$', name))
 
+def last_name(name):
+    return bool(re.fullmatch(r'^[A-Z][a-zA-Z]{2,}$', name))
+
 def main():
     first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
 
-    if first_name(first_name):
-        print("Valid first name!")
+    if first_name(first_name) and last_name(last_name):
+        print("Valid first and last name!")
     else:
-        print("Invalid name. First name must start with a capital letter and have at least 3 characters.")
+        print("Invalid name. First and last names must start with a capital letter and have at least 3 characters.")
 
 
 if __name__ == "__main__":
