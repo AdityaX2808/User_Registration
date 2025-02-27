6036ee5ba02edb1fcf1d52430f5c37076e57c6a2
Adityax2808 <ii3837@srmist.edu.in>
2025-02-27 17:16:32 +0530

    [Aditya] Refactor: refactored the code because of errors

--- user_registration.py
+++ user_registration.py
@@ -1,20 +1,20 @@
 import re
 
-def first_name(name):
-    return bool(re.fullmatch(r'^[A-Z][a-zA-Z]{2,}$', name))
+def is_valid_first_name(name):
+    return bool(re.fullmatch(r'^[A-Z][a-zA-Z ]{2,}$', name))
 
-def last_name(name):
-    return bool(re.fullmatch(r'^[A-Z][a-zA-Z]{2,}$', name))
+def is_valid_last_name(name):
+    return bool(re.fullmatch(r'^[A-Z][a-zA-Z ]{2,}$', name))
 
 def main():
     first_name = input("Enter your first name: ").strip()
     last_name = input("Enter your last name: ").strip()
 
-    if first_name(first_name) and last_name(last_name):
+    if is_valid_first_name(first_name) and is_valid_last_name(last_name):  # Correct function calls
         print("Valid first and last name!")
     else:
         print("Invalid name. First and last names must start with a capital letter and have at least 3 characters.")
 
-
+# Run the main function
 if __name__ == "__main__":
     main()
